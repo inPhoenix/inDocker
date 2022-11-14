ea3ef7bc00f44bb23229e8a2c4b12f8e9e11a011
inPhoenix <spiderk@gmail.com>
2022-11-14 12:12:59 -0300

    :city_sunset: project updates

--- App/index.js
+++ App/index.js
@@ -1,24 +1,23 @@
-import express from 'express'
-import redis from 'redis'
-const app = express()
+import express from "express";
+import redis from "redis";
+const app = express();
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 const client = redis.createClient({
-    host: 'redis-server', // docker container address
-    port: 6379
-})
+  host: "redis-server", // docker container address
+  port: 6379,
+});
 
-client.set('visits', 0)
+client.set("visits", 0);
 
-app.get('/', (req, res) => {
-    client.get('visits', (err, visits) => {
-        res.send('nUmber of Visits is' + visits)
-        client.set('visits', parseInt(visits) + 1)
-    })
-})
+app.get("/", (req, res) => {
+  client.get("visits", (err, visits) => {
+    res.send(`Number of visit on this page is: ${visits}`);
+    client.set("visits", parseInt(visits) + 1);
+  });
+});
 
-app.listen(port,() => {
-    console.log('server started with Redis')
-})
-
+app.listen(port, () => {
+  console.log("server started with Redis");
+});
